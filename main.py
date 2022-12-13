0105d7a55f43998f8cc5f934b3c9fc862cf00972
dmitriikuleshov <dmkuleshov10@gmail.com>
2022-12-13 18:20:18 +0300

    Second Commit Капучино

--- main.py
+++ main.py
@@ -17,32 +17,82 @@ def get_data():
     return result
 
 
+class ChangeWidget(QWidget):
+    def __init__(self, main_wdg_obj):
+        super().__init__()
+
+        uic.loadUi("addEditCoffeeForm.ui", self)
+        # self.setupUi(self)
+        self.main_wdg_obj = main_wdg_obj
+        self.setWindowTitle('Редактирование')
+        self.confirmPushButton.clicked.connect(self.confirm)
+
+    def confirm(self):
+        try:
+            self.errorLabel.setText('')
+            data = get_data()
+            id = self.idLineEdit.text()
+            sort = self.sortLineEdit.text()
+            if not sort:
+                raise Exception('Ошибка при заполнении форм')
+
+            roasting = self.roastingLineEdit.text()
+            if not roasting:
+                raise Exception('Ошибка при заполнении форм')
+
+            grains = self.grainsLineEdit.text()
+            if not grains:
+                raise Exception('Ошибка при заполнении форм')
+
+            taste = self.tasteLineEdit.text()
+            if not taste:
+                raise Exception('Ошибка при заполнении форм')
+
+            price = self.priceLineEdit.text()
+            if not price:
+                raise Exception('Ошибка при заполнении форм')
+
+            size = self.sizeLineEdit.text()
+            if not size:
+                raise Exception('Ошибка при заполнении форм')
+
+            print(data)
+            if int(id) in [elem[0] for elem in data]:
+                cursor.execute(f"""UPDATE coffee SET
+                                "Название сорта" = '{sort}', "Степень обжарки" = '{roasting}',
+                                 "Молотый/в зернах" = '{grains}', "Описание вкуса" = '{taste}',
+                                  "Цена" = '{price}', "Объем упаковки" = '{size}'
+                                                WHERE "id" = '{id}'""")
+            else:
+                cursor.execute(f"""INSERT INTO coffee("id", "Название сорта", "Степень обжарки",
+                        "Молотый/в зернах", "Описание вкуса", "Цена", "Объем упаковки")
+                            VALUES('{id}', '{sort}', '{roasting}', '{grains}', '{taste}', '{price}', '{size}')""")
+
+            self.main_wdg_obj.fill_table()
+            self.close()
+
+        except Exception as e:
+            self.errorLabel.setText('Ошибка при заполнении форм')
+            print(e)
+
+
 class MyWidget(QWidget):
     def __init__(self):
         super().__init__()
         uic.loadUi("main.ui", self)
         # self.setupUi(self)
-        self.setWindowTitle("Эспрессо")
-
-        # self.add_widget = AddWidget(self)
-        # self.add_widget.setWindowFlags(Qt.Window | Qt.WindowStaysOnTopHint)
-        # self.addButton.clicked.connect(self.add_coffee)
-        #
-        # self.change_widget = ChangeWidget(self)
-        # self.change_widget.setWindowFlags(Qt.Window | Qt.WindowStaysOnTopHint)
-        # self.changeButton.clicked.connect(self.change_coffee)
-        #
-        # self.del_widget = DelWidget(self)
-        # self.del_widget.setWindowFlags(Qt.Window | Qt.WindowStaysOnTopHint)
-        # self.delButton.clicked.connect(self.del_coffee)
+        self.setWindowTitle("Капучино")
 
         self.addButton.setVisible(False)
-        self.changeButton.setVisible(False)
         self.delButton.setVisible(False)
+
+        self.changeButton.clicked.connect(self.change_coffee)
 
         self.tableWidget.setColumnCount(len(columns_names))
         self.tableWidget.setHorizontalHeaderLabels(columns_names)
         self.tableWidget.setVerticalHeaderLabels([str(i) for i in range(1, len(get_data()))])
+
+        self.change_widget = ChangeWidget(self)
 
         self.fill_table()
 
@@ -56,23 +106,14 @@ class MyWidget(QWidget):
         except Exception as e:
             print(e)
 
-    # def add_coffee(self):
-    #     self.close_all_widgets()
-    #     self.add_film_widget.show()
-    #
-    # def change_coffee(self):
-    #     self.close_all_widgets()
-    #     self.change_film_widget.show()
-    #
-    # def del_coffee(self):
-    #     self.close_all_widgets()
-    #     self.del_film_widget.show()
+    def change_coffee(self):
+        self.change_widget.close()
+        self.change_widget.show()
 
-    # def closeEvent(self, QCloseEvent):
-    #     self.add_widget.close()
-    #     self.change_widget.close()
-    #     connect.commit()
-    #     connect.close()
+    def closeEvent(self, QCloseEvent):
+        self.change_widget.close()
+        connect.commit()
+        connect.close()
 
 
 if __name__ == '__main__':
